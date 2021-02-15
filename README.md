0135f8f8b8201dbc3b44bc5a161f7294f0864111
Sean <srafferty89@gmail.com>
2021-02-15 18:34:54 +0000

    basic console app added!

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CosmosCore
+
 Sandbox to test out azure cosmos DB
+
+## CosmosConsole
+
+Following [link](https://docs.microsoft.com/en-us/azure/cosmos-db/sql-api-get-started)
---
+++ cosmosconsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Cosmos;
+
+namespace cosmosconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // ADD THIS PART TO YOUR CODE
+
+            // The Azure Cosmos DB endpoint for running this sample.
+            private static readonly string EndpointUri = "<your endpoint here>";
+        // The primary key for the Azure Cosmos account.
+        private static readonly string PrimaryKey = "<your primary key>";
+
+        // The Cosmos client instance
+        private CosmosClient cosmosClient;
+
+        // The database we will create
+        private Database database;
+
+        // The container we will create.
+        private Container container;
+
+        // The name of the database and container we will create
+        private string databaseId = "FamilyDatabase";
+        private string containerId = "FamilyContainer";
+    }
+}
+}
